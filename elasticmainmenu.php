8bf958c0791ee043cbfde889fc95359833cb1db7
Mateusz <mateusz.stronydlafirm@gmail.com>
2024-12-21 22:58:19 +0100

    First commit

---
+++ elasticmainmenu.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+use Module\ElasticMainmenu\Database\DbInstaller;
+
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+if (file_exists(__DIR__.'/vendor/autoload.php')) {
+    require_once __DIR__.'/vendor/autoload.php';
+}
+
+class ElasticMainmenu extends Module
+{
+    public function __construct()
+    {
+
+        // $loadedClasses = get_declared_classes();
+        // dump($loadedClasses);
+        $this->name = 'elasticmainmenu';
+        $this->author = '4AD STUDIO';
+        $this->version = '1.0.0';
+        $this->ps_versions_compliancy = ['min' => '1.7.7', 'max' => '8.99.99'];
+
+        parent::__construct();
+
+        $this->displayName = $this->l('ElasticMainMenu');
+        $this->description = $this->l('This module allows you to easily compose and speed up your top nav.');
+    }
+
+    public function install()
+    {
+        return $this->installTables() && parent::install() && $this->registerHook('displayHome');
+    }
+
+    public function uninstall()
+    {
+        return $this->removeTables() && parent::uninstall();
+    }
+
+    private function installTables()
+    {
+        /** @var DbInstaller $installer */
+        $installer = $this->getInstaller();
+        $errors = $installer->createTables();
+
+        return empty($errors);
+    }
+
+    private function removeTables()
+    {
+        /** @var DbInstaller $installer */
+        $installer = $this->getInstaller();
+        $errors = $installer->dropTables();
+
+        return empty($errors);
+    }
+
+    private function getInstaller()
+    {
+       
+
+        try { 
+            $installer = $this->get('prestashop.module.elasticmainmenu.db.install');
+        } catch (Exception $e) {
+            // Catch exception in case container is not available, or service is not available
+            $installer = null;
+        }
+
+        // During install process the modules's service is not available yet so we build it manually
+        if (!$installer) {
+            $installer = new DbInstaller(
+                $this->get('doctrine.dbal.default_connection'),
+                $this->getContainer()->getParameter('database_prefix')
+            );
+        }
+
+        return $installer;
+    }
+}
---
+++ src/Database/DbInstaller.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * 2007-2020 PrestaShop SA and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0).
+ * It is also available through the world-wide-web at this URL: https://opensource.org/licenses/AFL-3.0
+ */
+declare(strict_types=1);
+
+namespace Module\ElasticMainmenu\Database;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Driver\Statement;
+
+/**
+ * We cannot use Doctrine entities on install because the mapping is not available yet
+ * but we can still use Doctrine connection to perform DQL or SQL queries.
+ */
+class DbInstaller
+{
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * @var string
+     */
+    private $dbPrefix;
+
+    /**
+     * @param Connection $connection
+     * @param string $dbPrefix
+     */
+    public function __construct(
+        Connection $connection,
+        $dbPrefix
+    ) {
+        $this->connection = $connection;
+        $this->dbPrefix = $dbPrefix;
+    }
+
+    /**
+     * @return array
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function createTables()
+    {
+
+        
+        $errors = [];
+        $this->dropTables();
+        $sqlInstallFile = __DIR__ . '/../../Resources/data/install.sql';
+        $sqlQueries = preg_split('/\r\n|\r|\n/', file_get_contents($sqlInstallFile));
+        $sqlQueries = str_replace('PREFIX_', $this->dbPrefix, $sqlQueries);
+
+        foreach ($sqlQueries as $query) {
+            if (empty($query)) {
+                continue;
+            }
+            $statement = $this->connection->executeQuery($query);
+            if (0 != (int) $statement->errorCode()) {
+                $errors[] = [
+                    'key' => json_encode($statement->errorInfo()),
+                    'parameters' => [],
+                    'domain' => 'Admin.Modules.Notification',
+                ];
+            }
+        }
+
+        return $errors;
+    }
+
+    /**
+     * @return array
+     *
+     * @throws DBALException
+     */
+    public function dropTables()
+    {
+        $errors = [];
+        $tableNames = [
+            'elasticmainmenu'
+        ];
+        foreach ($tableNames as $tableName) {
+            $sql_table = 'DROP TABLE IF EXISTS ' . $this->dbPrefix . $tableName;
+            $statement_table = $this->connection->executeQuery($sql_table);
+            if ($statement_table instanceof Statement && 0 != (int) $statement_table->errorCode()) {
+                $errors[] = [
+                    'key' => json_encode($statement_table->errorInfo()),
+                    'parameters' => [],
+                    'domain' => 'Admin.Modules.Notification',
+                ];
+            }
+        }
+
+        $checkIfColumnsExist = $this->connection->executeQuery("SHOW COLUMNS FROM ps_category_lang LIKE 'emm_position'");
+        if ($checkIfColumnsExist->rowCount() > 0) {
+            $this->connection->executeQuery("ALTER TABLE ".$this->dbPrefix."category_lang DROP COLUMN emm_position, DROP COLUMN emm_enabled");
+        }
+
+
+
+        return $errors;
+    }
+}
