9cf66c6a4468cded3a070345e560d2e72a404614
Mateusz <mateusz.stronydlafirm@gmail.com>
2024-12-24 14:13:04 +0100

    Test display products

--- src/Controller/Admin/ElasticmainmenuController.php
+++ src/Controller/Admin/ElasticmainmenuController.php
@@ -3,21 +3,32 @@ declare(strict_types=1);
 
 namespace Module\ElasticMainmenu\Controller\Admin;
 
-use Module\DemoDoctrine\Grid\Filters\QuoteFilters;
+use Module\ElasticMainmenu\Grid\Definition\Factory\ProductGridDefinitionFactory;
+use Module\ElasticMainmenu\Grid\Filters\ProductFilters;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use Symfony\Component\HttpFoundation\Response;
 
 class ElasticmainmenuController extends FrameworkBundleAdminController
 {
-    public function indexAction()
+    /**
+     * List quotes
+     *
+     * @param ProductFilters $filters
+     *
+     * @return Response
+     */
+    public function indexAction(ProductFilters $filters)
     {
+        $quoteGridFactory = $this->get('demo_grid.grid.factory.products');
+        $quoteGrid = $quoteGridFactory->getGrid($filters);
+
         return $this->render(
             '@Modules/elasticmainmenu/views/templates/admin/index.html.twig',
             [
                 'enableSidebar' => true,
                 'layoutTitle' => $this->trans('Elastic MainMenu', 'Modules.Elasticmainmenu.Admin'),
                 // 'layoutHeaderToolbarBtn' => $this->getToolbarButtons(),
-                // 'quoteGrid' => $this->presentGrid($quoteGrid),
+                'quoteGrid' => $this->presentGrid($quoteGrid),
             ]
         );
     }
